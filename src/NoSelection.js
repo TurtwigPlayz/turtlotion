a1762991fd30f150b5aac2df083b4ef51b9be991
TurtwigPlayz <Nelsonthompson2003@gmail.com>
2023-03-01 13:35:53 -0700

    Small changes to help with successful deployment

--- src/NoSelection.js
+++ src/NoSelection.js
@@ -1,9 +1,9 @@
 function NoSelection() {
-    return (
-      <div id="no-note">
-        Select a note, or create a new one.
-      </div>
-    );
-  }
-  
-  export default NoSelection;
+  return (
+    <div id="no-note">
+      Select a note, or create a new one.
+    </div>
+  );
+}
+
+export default NoSelection;
